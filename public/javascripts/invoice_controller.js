bd9fa9d7f69d23972e378be8101ef99758381822
Anton Holmquist <anton.holmquist@gmail.com>
2013-06-15 20:57:38 +0200

    Can now add and remove items with angular

--- public/javascripts/invoice_controller.js
+++ public/javascripts/invoice_controller.js
@@ -5,4 +5,20 @@ function InvoiceController($scope) {
 		{description:'en apa', price:500},
 		{description:'en ko', price:1500},
 	];
+
+	$scope.addItem = function() {
+		var item = {
+			description: $scope.description,
+			price: $scope.price,
+		}
+		$scope.items.push(item);
+
+		$scope.description = '';
+		$scope.price = '';
+	};
+
+	$scope.removeItem = function(index) {
+		$scope.items.splice(index, 1);
+	};
 }
+
