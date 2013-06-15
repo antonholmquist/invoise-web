1c172b8ca51cdfbc50a20679641ed8c3acf944bd
Anton Holmquist <anton.holmquist@gmail.com>
2013-06-15 21:33:14 +0200

    Total and tax now calculated

--- public/javascripts/invoice_controller.js
+++ public/javascripts/invoice_controller.js
@@ -1,10 +1,36 @@
 
 
 function InvoiceController($scope) {
+
+	
+	$scope.currencySymbol = '$';
+	$scope.vatPercentage = '0.25';
+
 	$scope.items = [
 		{description:'en apa', price:500},
 		{description:'en ko', price:1500},
 	];
+
+	$scope.updateSummary = function() {
+
+		var subtotalPrice = 0;
+
+		for (i in $scope.items) {
+			var item = $scope.items[i];
+			subtotalPrice += parseInt(item.price, 10);
+		}
+		
+		$scope.subtotalPrice = subtotalPrice;
+		$scope.vatAmount = $scope.subtotalPrice * $scope.vatPercentage;
+		
+
+	$scope.totalPrice = $scope.subtotalPrice + $scope.vatAmount;
+
+	}; $scope.updateSummary();
+
+	$scope.updateTo = function() {
+		$scope.totalPrice = 0;
+	}; $scope.updateSummary();
 
 	$scope.addItem = function() {
 		var item = {
@@ -15,11 +41,14 @@ function InvoiceController($scope) {
 
 		$scope.description = '';
 		$scope.price = '';
+
+		$scope.updateSummary();
 	};
 
 	$scope.removeItem = function(index) {
 		console.log("remove");
 		$scope.items.splice(index, 1);
+		$scope.updateSummary();
 	};
 }
 
