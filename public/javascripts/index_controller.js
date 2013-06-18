547baef330cf4f47cc9192ec8fb5ea95edf320a9
Anton Holmquist <anton.holmquist@gmail.com>
2013-06-18 01:10:26 +0000

    Added spin.js

--- public/javascripts/index_controller.js
+++ public/javascripts/index_controller.js
@@ -22,8 +22,29 @@ $(document).ready(function() {
 		});
 	});
 
-	$("button#downloadPDF").click(function(event) {		
+	$("button#downloadPDF").click(function(event) {
+
+var opts = {
+  lines: 13, // The number of lines to draw
+  length: 15, // The length of each line
+  width: 4, // The line thickness
+  radius: 18, // The radius of the inner circle
+  corners: 1, // Corner roundness (0..1)
+  rotate: 0, // The rotation offset
+  direction: 1, // 1: clockwise, -1: counterclockwise
+  color: '#eee', // #rgb or #rrggbb
+  speed: 1, // Rounds per second
+  trail: 60, // Afterglow percentage
+  top: 100, // Top position relative to parent in px
+  left: -50 // Left position relative to parent in px
+};
+			
+		//var spinner = new Spinner(opts).spin();
+		//this.appendChild(spinner.el);	
+		//$("button#downloadPDF").hide();
 		pdfRequest(globalInvoice, function(response) {
+			$("button#downloadPDF").show();
+			//spinner.stop();
 			window.location.href = response.downloadURL;
 		});
 	});
